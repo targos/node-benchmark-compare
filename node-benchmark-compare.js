8f65082fb9e7bee76a9e7beeff297f570b71b864
Mestery <mestery@pm.me>
2021-08-13 08:30:52 +0200

    fix: support csv parsing when there is a comma inside quotes (#1)

--- node-benchmark-compare.js
+++ node-benchmark-compare.js
@@ -10,16 +10,66 @@ if (process.argv.length !== 3) {
   process.exit(1);
 }
 
+const State = {
+  NONE: 0,
+  STRING: 1,
+  NUMBER: 2,
+}
+
+function parseCsvLine(data, row) {
+  let state = State.NONE;
+  let escaped = false;
+  let tmp = '';
+  const items = [];
+
+  let i = 0;
+  for (const char of data) {
+    if (
+      char === '\\' &&
+      (state !== State.STRING || data[i + 1] === '"') &&
+      (escaped = !escaped)
+    ) {
+      i++;
+      continue;
+    }
+
+    if (state !== State.STRING && char.trim() === '') {
+      // ignore whitespaces
+    } else if (state === State.NONE && char === '"') {
+      state = State.STRING;
+    } else if (state === State.STRING && char === '"') {
+      if (escaped) {
+        tmp += '"';
+      } else {
+        state = State.NONE;
+      }
+    } else if (state !== State.STRING && char === ',') {
+      items.push(state === State.NUMBER ? Number(tmp) : tmp);
+      state = State.NONE;
+      tmp = '';
+    } else if (state === State.NONE) {
+      state = State.NUMBER;
+      tmp = char;
+    } else {
+      tmp += char;
+    }
+
+    escaped = false;
+    i++;
+  }
+
+  if (tmp) {
+    items.push(state === State.NUMBER ? Number(tmp) : tmp);
+  }
+
+  return items;
+}
+
 // Parse the CSV file.
 const rawData = readFileSync(process.argv[2], 'utf8')
   .split('\n')
   .filter((line) => line.trim() !== '')
-  .map((line) => {
-    return line
-      .trim()
-      .split(',')
-      .map((cell) => JSON.parse(cell));
-  });
+  .map(parseCsvLine);
 
 // Transform the data into objects.
 const [headers] = rawData;
